b7cc586bb8b6abc609e018f8d8ee5e2cb93a075b
Shanitkk <Shanitk2020@gmail.com>
2022-11-11 22:18:04 +0200

    Initial commit

---
+++ Game.js
@@ -0,0 +1,58 @@
+function go()
+{
+    document.
+
+}
+
+
+
+
+
+
+
+
+function addLettersButton() {
+    const keys = [
+        'א' +
+        'ב' +
+        'ג' +
+        'ד' +
+        'ה' +
+        'ו' +
+        'ז' +
+        'ח' +
+        'ט' +
+        'י' +
+        'כ' +
+        'ל' +
+        'מ' +
+        'נ' +
+        'ס' +
+        'ע' +
+        'פ' +
+        'צ' +
+        'ק' +
+        'ר' +
+        'ש' +
+        'ת' +
+        'ף' +
+        'ך' +
+        'ץ' +
+        'ם' +
+        'Enter'
+    ]
+
+    for (let i = 0; i < keys.length; i++) {
+        const buttonElement = document.createElement('button')
+        buttonElement.textContent = key
+
+    }
+}
+
+const guessRows = [
+    '', '', '', '', '',
+    '', '', '', '', '',
+    '', '', '', '', '',
+    '', '', '', '', '',
+    '', '', '', '', '',
+    '', '', '', '', '']
