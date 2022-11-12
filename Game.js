1b40df628f1722d78b4620893fa0d83bc412f5e1
Shanitkk <Shanitk2020@gmail.com>
2022-11-12 15:15:58 +0200

    1

--- Game.js
+++ Game.js
@@ -1,15 +1,37 @@
-function go()
-{
-    document.
 
+function randomWordToGuess() {
+    const words = [
+        "חביתה",
+        "ילקוט",
+        "ענבים",
+        "עוגיה",
+        "דגנים", //5
+        "מאורר",
+        "שמיכה",
+        "מחברת",
+        "שולחן",
+        "מסגרת",//10
+        "תמונה",
+        "שרשרת",
+        "גומיה",
+        "חולצה",
+        "משאית",//15
+        "מונית",
+        "רמזור",
+        "בקבוק",
+        "נימוס",
+        "ציפור"]
+    let randomIndex = Math.ceil(Math.random() * 20);
+    let randomWord = words[randomIndex];
+    alert(randomWord)
 }
 
+function checkWord()
+{
+    // let l0 = document.().type.concat(0);
+    // alert(l0)
 
-
-
-
-
-
+}
 
 function addLettersButton() {
     const keys = [
